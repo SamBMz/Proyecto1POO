58e3587b701ba6509dd27ac1ae7366cb3d8ed2b2
YizusDP <venomweblol@gmail.com>
2023-04-01 14:36:39 -0600

    Modificación agregando la especific. del cliente

--- Sucursal.java
+++ Sucursal.java
@@ -1,25 +1,29 @@
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
+
 public class Sucursal {
     int numSucursal;
     HashMap<String, Cliente> clientes;
     Administrador admin;
-    public Sucursal(){
+
+    public Sucursal() {
         Random rnd = new Random();
         admin = new Administrador(this);
-        numSucursal = rnd.nextInt(999)+99;
+        numSucursal = rnd.nextInt(999) + 99;
         clientes = new HashMap<>();
-        clientes.put("Cliente1", new Cliente("Clientes1"));
-        clientes.put("Cliente2", new Cliente("Clientes2"));
-        clientes.put("Cliente3", new Cliente("Clientes3"));
-        clientes.put("Cliente4", new Cliente("Clientes4"));
-        clientes.put("Cliente5", new Cliente("Clientes5"));
+        /*clientes.put("Cliente1", new Cliente("Cliente1"));
+        clientes.put("Cliente2", new Cliente("Cliente2"));
+        clientes.put("Cliente3", new Cliente("Cliente3"));
+        clientes.put("Cliente4", new Cliente("Cliente4"));
+        clientes.put("Cliente5", new Cliente("Cliente5"));*/
     }
-    public Set<String> getNombresClientes(){
+
+    public Set<String> getNombresClientes() {
         return clientes.keySet();
     }
-    public int getNumSucursal(){
+
+    public int getNumSucursal() {
         return numSucursal;
     }
 }
