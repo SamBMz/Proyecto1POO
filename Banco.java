57d9f893f08f949e63fb4d0ecf32cff33cfb43db
YizusDP <venomweblol@gmail.com>
2023-04-01 14:36:45 -0600

    Git add modificación de la clase banco

--- Banco.java
+++ Banco.java
@@ -2,44 +2,79 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class Banco {
     int numSucursales;
-    List<HashMap<String, Object>> sucursales; // Cambio de Sucursal a HashMap<String, Object>
+    List<HashMap<String, Object>> sucursales;
+    HashMap<String, Cliente> clientes;
 
     public Banco() {
         sucursales = new ArrayList<>();
         for (int i = 0; i < 3; i++)
-            sucursales.add(new HashMap<String, Object>()); // Crear HashMap vacío
+            sucursales.add(new HashMap<String, Object>());
+        clientes = new HashMap<>();
     }
 
     public Banco(int numSucursales) {
         this.numSucursales = numSucursales;
         sucursales = new ArrayList<>();
         while (numSucursales-- > 0)
-            sucursales.add(new HashMap<String, Object>()); // Crear HashMap vacío
+            sucursales.add(new HashMap<String, Object>());
+        clientes = new HashMap<>();
     }
 
     public void agregarSucursal() {
-        sucursales.add(new HashMap<String, Object>()); // Crear HashMap vacío
+        sucursales.add(new HashMap<String, Object>());
     }
 
     public int getNumSucursales() {
         return numSucursales;
     }
 
-    public HashMap<String, Object> getSucursal(int indexSucursal) { // Cambio de Sucursal a HashMap<String, Object>
+    public HashMap<String, Object> getSucursal(int indexSucursal) {
         return sucursales.get(indexSucursal);
     }
 
-    public HashMap<String, Object> getSucursal() { // Cambio de Sucursal a HashMap<String, Object>
+    public HashMap<String, Object> getSucursal() {
         Random rnd = new Random();
         return sucursales.get(rnd.nextInt(numSucursales));
     }
 
     public void mostrarSucursales() {
         for (int i = 0; i < numSucursales; i++) {
-            System.out.println(i + "-Sucursal " + sucursales.get(i).get("numSucursal")); // Obtener el valor de la clave "numSucursal" del HashMap
+            System.out.println(i + "-Sucursal " + sucursales.get(i).get("numSucursal"));
+        }
+    }
+
+    public Set<String> getNombresClientes() {
+        return clientes.keySet();
+    }
+
+    public Cliente getCliente(String nombreCliente) {
+        return clientes.get(nombreCliente);
+    }
+
+    public void agregarCliente(String nombreCliente) {
+        clientes.put(nombreCliente, new Cliente(nombreCliente));
+    }
+
+    public void modificarCliente(String nombreCliente, String nuevoNombre) {
+        Cliente cliente = clientes.get(nombreCliente);
+        clientes.remove(nombreCliente);
+        cliente.setNombre(nuevoNombre);
+        clientes.put(nuevoNombre, cliente);
+    }
+
+    public void mostrarCliente(String nombreCliente) {
+        Cliente cliente = clientes.get(nombreCliente);
+        if (cliente != null) {
+            System.out.println("Nombre: " + cliente.getNombre());
+            System.out.println("Edad: " + cliente.getEdad());
+            System.out.println("Direccion: " + cliente.getDireccion());
+            System.out.println("Telefono: " + cliente.getTelefono());
+        } else {
+            System.out.println("Cliente no encontrado.");
         }
     }
 
